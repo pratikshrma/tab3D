388a04673426619efd0afb96512add79ac33fa72
Pratik <amnshrma341@gmail.com>
2025-12-12 17:28:59 +0530

    feat: removed controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Components/Experience";
 import style from "./Styles/App.module.css";
-import { Perf } from "r3f-perf";
 import PostProcessing from "./Components/PostProcessing";
+import { Leva } from "leva";
 
 const App = () => {
   return (
@@ -18,18 +18,19 @@ const App = () => {
         </div>
       </div>
       <Canvas
+        dpr={1}
         camera={{
-          fov:35,
+          fov: 35,
           position: [0, 2, 15],
         }}
         gl={{
           alpha: true,
         }}
       >
-        <Perf position="top-left"/>
-          <PostProcessing/>
-          <Experience />
-
+        <Leva hidden/>
+        <color attach="background" args={["#F7E5CD"]} />
+        <PostProcessing />
+        <Experience />
       </Canvas>
       <div className="vignette"></div>
     </div>
