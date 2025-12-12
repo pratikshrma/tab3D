41e41cc7ab3e02debe8aceeb4b8a581002426a80
Pratik <amnshrma341@gmail.com>
2025-12-12 17:32:14 +0530

    feat: removed backgorund circle

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Components/Experience";
-import style from "./Styles/App.module.css";
 import PostProcessing from "./Components/PostProcessing";
 import { Leva } from "leva";
 
 const App = () => {
   return (
     <div className="canvas-container">
-      <div>
-        <div className={style.ringContainer}>
-          <div className={style.ring1}>
-            <img src="/assets/ellipse1.svg" />
-          </div>
-          <div className={style.ring2}>
-            <img src="/assets/ellipse2.svg" />
-          </div>
-        </div>
-      </div>
       <Canvas
         dpr={1}
         camera={{
